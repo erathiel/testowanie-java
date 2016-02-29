822f7abef4f15a2f7b180c9a4ffa0cea35d66fa7
Amelia Błachuciak <ablachuciak@p29-14.labpk.inf.ug.edu.pl>
2016-02-29 12:53:05 +0100

    Added wejsciowka01

---
+++ wejsciowka01/src/main/java/com/example/app/garden/GardenManager.java
@@ -0,0 +1,5 @@
+package com.example.app.garden;
+
+public class GardenManager {
+
+}
---
+++ wejsciowka01/src/test/java/com/example/app/garden/GardenManagerTest.java
@@ -0,0 +1,20 @@
+package com.example.app.garden;
+
+import junit.framework.Test;
+
+public class GardenManagerTest
+{
+    
+	Garden garden1 = new Garden("Fiolek", 5);
+	Garden garden2 = new Garden("Roza", 3);
+	
+	@Test
+	public void addCheck() {
+		//assertEquals()
+	}
+	
+	@Test
+    public void removeCheck() {
+    	//assertEquals()
+    }
+}
